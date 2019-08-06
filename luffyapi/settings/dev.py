5cc6148a0e4676ceebd5d1be172c16115b770da1
wangsijie <2245713938@qq.com>
2019-08-06 19:51:39 +0800

    创建数据库并修改数据库配置，同时运行时，解决版本和编码两个bug

--- luffyapi/settings/dev.py
+++ luffyapi/settings/dev.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'luffyapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'luffy',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': '123',
     }
 }
 
